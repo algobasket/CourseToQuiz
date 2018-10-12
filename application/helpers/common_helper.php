aec9dd9db260dc773fc132cdf6b67e39bebf3b51
algobasket <algobasket@gmail.com>
2018-10-12 09:27:46 +0530

    test

--- application/helpers/common_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
- if( ! function_exists('jsonPreetify')){
-   function jsonPreetify($data){ 
-     return json_encode($data,JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-   }
- }
-
- ?>
